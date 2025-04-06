2f237ba2f22931690fe2cdd7c5db5a12408653d6
Shreya Parkar <shreya.parkar.197@gmail.com>
2025-04-06 21:35:47 +0530

    Added google oauth in server

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -2,8 +2,15 @@ from dotenv import load_dotenv
 import os
 load_dotenv('../../.env')
 
+# Loading redis environment variables
 host = os.getenv("REDIS_HOST")
 username = os.getenv("REDIS_USERNAME")
 password = os.getenv("REDIS_PASSWORD")
+
+# Loading mongodb environment variables
 port = os.getenv("REDIS_PORT")
 db_url = os.getenv("MONGODB")
+
+# Loading google oauth environment variables
+client_id = os.getenv("GOOGLE_CLIENT_ID")
+client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
--- server/main.py
+++ server/main.py
@@ -1,3 +1,27 @@
 from app.core.connect_mongo import mongo_connect
+from app.core.connect_oauth import oauth_connect
+from flask import Flask, redirect, url_for, session
 
-mongo_connect()
+app = Flask(__name__)
+app.secret_key = 'aksdwodkfjksdfjwo'
+
+db = mongo_connect()
+google = oauth_connect(app)
+
+@app.route('/')
+def home():
+    return '<a href="/login">Login with Google</a>'
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth_callback', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/auth/callback')
+def auth_callback():
+    token = google.authorize_access_token()
+    user_info = google.get('userinfo').json()
+    return f'Hello {user_info["name"]}! Your email is {user_info["email"]}.'
+
+if __name__ == '__main__':
+    app.run(debug=True)

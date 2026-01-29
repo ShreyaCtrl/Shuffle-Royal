b6a123078d9dded8e20305738a4f6412b9b7ab5b
Shreya Parkar <shreya.parkar.197@gmail.com>
2026-01-30 00:27:09 +0530

    Dummy navbar created

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=''>
+      <nav className="flex">
+        <div style={{color:"var(--accent-primary)"}} className='p-2'>Home</div>
+        <div style={{color:"var(--accent-primary)"}} className='p-2'>Rooms</div>
+        <div style={{color:"var(--accent-primary)"}} className='p-2'>Profile</div>
+        <div style={{color:"var(--accent-primary)"}} className='p-2'>Themes</div>
+        <div style={{color:"var(--accent-primary)"}} className='p-2'>Leaderboard</div>
+      </nav>
+    </div>
   )
 }
 
